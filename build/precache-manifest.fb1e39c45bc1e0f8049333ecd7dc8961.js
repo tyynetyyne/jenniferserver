197ebf984b81a17a3a7fb8c0ce5bafb0d1246de2
tyynetyyne <tyynetyyne@gmail.com>
2019-07-17 19:58:10 +0300

    frontend

---
+++ build/precache-manifest.fb1e39c45bc1e0f8049333ecd7dc8961.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba5a9da98df6d2434eee340c299e15a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b63e6a2dda3577c9143",
+    "url": "/static/css/main.fa237f00.chunk.css"
+  },
+  {
+    "revision": "7d527b7a692c15225b04",
+    "url": "/static/js/2.283eb7fd.chunk.js"
+  },
+  {
+    "revision": "3b63e6a2dda3577c9143",
+    "url": "/static/js/main.6f0be237.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ff0209f63b8666ab9ec2381759c79f75",
+    "url": "/static/media/Jennifer_banner.ff0209f6.png"
+  }
+]);
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const requestLogger = (request, response, next) => {
 app.use(bodyParser.json())
 app.use(requestLogger);
 app.use(cors());
+app.use(express.static('build'))
 
 let gigs = [ 
   {
